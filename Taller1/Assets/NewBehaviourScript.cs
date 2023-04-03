a54b26fd51cefee0938a802383622e415c799b05
JotaPe <jcamacho249@unab.edu.co>
2023-04-02 22:45:59 -0500

    Update rigidbody de juan

--- Taller1/Assets/NewBehaviourScript.cs
+++ Taller1/Assets/NewBehaviourScript.cs
@@ -1,13 +1,18 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
+    Rigidbody rb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+        rb.isKinematic = true;
+        rb.constraints = RigidbodyConstraints.FreezePositionZ | RigidbodyConstraints.FreezeRotationY;
+        rb.gameObject.AddComponent<BoxCollider>();
+        rb.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
