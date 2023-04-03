030c692eca5e0b5cb42358fd9759ab143166691d
Jorge Walter <jorgewalterpro@gmail.com>
2023-04-02 22:42:22 -0500

    Update NewBehaviourScript.cs

--- Taller1/Assets/NewBehaviourScript.cs
+++ Taller1/Assets/NewBehaviourScript.cs
@@ -7,7 +7,7 @@ public class NewBehaviourScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        GameObject go = new GameObject();
     }
 
     // Update is called once per frame
